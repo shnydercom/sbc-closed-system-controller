7a452f56fe558e55359265dd7a48b5dc2fb21683
shnyder <ajs.private@gmail.com>
2024-04-13 00:16:18 +0200

    servo can be switched over http from remote

--- control-web-server/app.py
+++ control-web-server/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import hardwarecom.rpi_gpio_servos as hwcom
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def root_url():
     return "<p>this is the server root</p>"
+
+
+@app.route("/switch")
+def switch():
+    newVal = hwcom.switch_high_low()
+    return f"<p>switched to {newVal}</p>"
