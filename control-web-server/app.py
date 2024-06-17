03a5d62d2bd4abe0c0baf4719f78c77dd37f76ea
shnyder <ajs.private@gmail.com>
2024-06-17 18:31:40 +0200

    basic setup of pwa

--- control-web-server/app.py
+++ control-web-server/app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, send_file
 import hardwarecom.rpi_servohat_pantilt_adafruit1967 as pantilt
 
 app = Flask(__name__)
 
 
+# User Interface resources (Progressive Web App)
+# following guide from: https://medium.com/@tristan_4694/how-to-create-a-progressive-web-app-pwa-using-flask-f227d5854c49
 @app.route("/")
 def root_url():
-    return "<p>this is the server root</p>"
+    return render_template("index.html")
+
+
+@app.route("/manifest.json")
+def serve_manifest():
+    return send_file("templates/manifest.json", mimetype="application/manifest+json")
+
+
+@app.route("/sw.js")
+def serve_sw():
+    return send_file("templates/sw.js", mimetype="application/javascript")
 
 
 # panning
@@ -58,3 +70,11 @@ def format_panning(angle):
 
 def format_tilting(angle):
     return f"<p>tilting angle: {angle}</p>"
+
+
+def init_hardware():
+    pantilt.pan_to_middle()
+    pantilt.tilt_to_middle()
+
+
+init_hardware()
