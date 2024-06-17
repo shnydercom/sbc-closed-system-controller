742e6f5dd6eb057c0ad561609d24e83142bc9a5e
shnyder <ajs.private@gmail.com>
2024-06-17 20:47:07 +0200

    MUI setup and some styling

--- client-web-app/vite.config.ts
+++ client-web-app/vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
+
+const PWAoptions: Partial<VitePWAOptions> = {
+	manifest: {
+		"name": "SBC closed system controller",
+		"short_name": "SBC CSC",
+		"start_url": "/",
+		"scope": "/",
+		"display": "standalone",
+		"theme_color": "#000000",
+		"background_color": "#000000",
+		"icons": [
+			{
+				"src": "/static/icons/icon_144x144.png",
+				"sizes": "144x144",
+				"type": "image/png",
+				"purpose": "any maskable"
+			},
+			{
+				"src": "/static/icons/icon_192x192.png",
+				"sizes": "192x192",
+				"type": "image/png",
+				"purpose": "any maskable"
+			},
+			{
+				"src": "/static/icons/icon_512x512.png",
+				"sizes": "512x512",
+				"type": "image/png",
+				"purpose": "any maskable"
+			}
+		]
+	}
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [VitePWA(PWAoptions), react()],
 })
